91ebf2a184621c2a07660a8ebf624ab67735d8fa
pratikdadmal <82523917+pratikdadmal@users.noreply.github.com>
2022-09-06 11:37:34 +0000

    Add files via upload

---
+++ commitdemo/src/main/java/sampledemo/commitdemo/Exceptioncommitdemo.java
@@ -0,0 +1,23 @@
+package sampledemo.commitdemo;
+
+public class Exceptioncommitdemo {
+	
+	public static void checkException(int age) throws Exception
+	{
+			if(age<18)
+			{
+				throw new ArithmeticException("User is not valid to vote");
+			}
+			else
+			{
+				System.out.println("user valid for vote");
+			}
+			
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		checkException(9);
+	}
+
+}
